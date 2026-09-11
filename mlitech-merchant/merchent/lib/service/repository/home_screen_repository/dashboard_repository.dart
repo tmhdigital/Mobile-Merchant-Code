6ec50c6a0706ef92da8a3186e91c859e4e4a072f
Samiullah Waheed <samiullahwaheed786@gmail.com>
2026-09-11 12:11:03 +0500

    authroization header error commented

--- mlitech-merchant/merchent/lib/service/repository/home_screen_repository/dashboard_repository.dart
+++ mlitech-merchant/merchent/lib/service/repository/home_screen_repository/dashboard_repository.dart
@@ -4,7 +4,7 @@ import 'package:merchent/screen/home_screen/model/dashboard_model.dart';
 import 'package:merchent/service/api_service/api_services.dart';
 import 'package:merchent/service/api_service/service_model/service_model.dart';
 
-import '../../../widget/app_snack_bar/app_snack_bar.dart';
+// import '../../../widget/app_snack_bar/app_snack_bar.dart';
 
 class MerchantReportRepository extends GetxController {
   bool _loading = false;
@@ -35,18 +35,17 @@ class MerchantReportRepository extends GetxController {
           return MerchantReportModel.fromJson(body);
         }
         _errorMessage = "Invalid response format received.";
-        AppSnackBar.error(_errorMessage!);
         return null;
       }
 
       _errorMessage = response.message;
-      AppSnackBar.error(response.message);
+      // AppSnackBar.error(response.message);
       return null;
     } catch (e) {
       _loading = false;
       _errorMessage = "Error fetching merchant report: ${e.toString()}";
       update();
-      AppSnackBar.error(_errorMessage!);
+      // AppSnackBar.error(_errorMessage!);
       return null;
     }
   }
